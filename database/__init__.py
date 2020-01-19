75a9d24bbf7d213bea8fbfc4e980f72ceed6a5bd
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-01-19 23:10:56 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -7,7 +7,7 @@ class Db:
 		
 	def name_exists(self, name):
 		cursor = self.connection.cursor()
-		cursor.execute("select name from eg1 where name=(%s)",[name])
+		cursor.execute("select name from details where name=(%s)",[name])
 		value = cursor.fetchall()
 		#this if condition to check name already is taken or not
 		if len(value) != 0:
@@ -15,25 +15,25 @@ class Db:
 		return False
 	def last_coupon_selecting(self):
 		cursor = self.connection.cursor()
-		cursor.execute("select coupon from eg1 order by id desc limit 1")
+		cursor.execute("select coupon from details order by id desc limit 1")
 		last_coupon=cursor.fetchall()
 		return last_coupon
 	def when_coupon_not_null(self):
 		cursor = self.connection.cursor()
-		cursor.execute("select coupon from eg1")
+		cursor.execute("select coupon from details")
 		refered_user=cursor.fetchall()
 		return refered_user
 	def registering(self,name,age,city,genrated_coupon):
 		cursor = self.connection.cursor()
-		cursor.execute("insert into eg1(name,age,city,coupon) values(%s,%s,%s,%s)",(name,age,city,genrated_coupon))
+		cursor.execute("insert into details(name,age,city,coupon) values(%s,%s,%s,%s)",(name,age,city,genrated_coupon))
 		self.connection.commit()
 	def refernce_table(self,coupon,name):
 		'''
 		this condition is to insert the referal and refree in table seprate table
-		here i can also use cur.execute("insert into refernce (referral)select name from eg1 where coupon=(%s))",[coupon]) this is subquery 
+		here i can also use cur.execute("insert into refernce (referral)select name from details where coupon=(%s))",[coupon]) this is subquery 
 		'''
 		cursor = self.connection.cursor()
-		cursor.execute("select name from eg1 where coupon=(%s)",[coupon])
+		cursor.execute("select name from details where coupon=(%s)",[coupon])
 		referral=cursor.fetchall()
 		'''
 		i did normal method to insert here
